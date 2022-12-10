32bf83df0e6b7633c16a6b1c27b7ada7ef9d9562
Ken-In <yinfuquan0130@foxmail.com>
2022-12-10 17:15:01 +0800

    add Window and GLFW.

--- Congb/src/Congb/EntryPoint.h
+++ Congb/src/Congb/EntryPoint.h
@@ -8,9 +8,6 @@ int main(int argc, char** argv)
 {
 	Congb::Log::Init();
 	CB_CORE_WARN("Initialized!");
-	int a = 5;
-	CB_INFO("Hello! Var = {0}", a);
-
 
 	auto app = Congb::CreateApplication();
 	app->Run();
