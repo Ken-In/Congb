afbc559ab632ff818f448472bc4777d83fe731c0
Ken-In <yinfuquan0130@foxmail.com>
2022-12-29 00:56:47 +0800

    add ShaderLayout.

--- Congb/src/Congb/Application.cpp
+++ Congb/src/Congb/Application.cpp
@@ -16,6 +16,27 @@ namespace Congb {
 
 	Application* Application::s_Instance = nullptr;
 
+	static GLenum ShaderDataTypeToOpenGLBaseType(ShaderDataType type)
+	{
+		switch (type)
+		{
+			case Congb::ShaderDataType::Float:	   return GL_FLOAT;
+			case Congb::ShaderDataType::Float2:	   return GL_FLOAT;
+			case Congb::ShaderDataType::Float3:	   return GL_FLOAT;
+			case Congb::ShaderDataType::Float4:	   return GL_FLOAT;
+			case Congb::ShaderDataType::Mat3:	   return GL_FLOAT;
+			case Congb::ShaderDataType::Mat4:	   return GL_FLOAT;
+			case Congb::ShaderDataType::Int:	   return GL_INT;
+			case Congb::ShaderDataType::Int2:	   return GL_INT;
+			case Congb::ShaderDataType::Int3:	   return GL_INT;
+			case Congb::ShaderDataType::Int4:	   return GL_INT;
+			case Congb::ShaderDataType::Bool:	   return GL_BOOL;
+		}
+
+		CB_CORE_ASSERT(false, "Unknown ShaderDataType!");
+		return 0;
+	}
+
 	Application::Application()
 	{
 		CB_CORE_ASSERT(!s_Instance, "Application already exists!");
@@ -33,15 +54,37 @@ namespace Congb {
 		glGenVertexArrays(1, &m_VertextArray);
 		glBindVertexArray(m_VertextArray);
 
-		float vertices[3 * 3] = {
-			-0.5f, -0.5f, 0.0f,
-			 0.5f, -0.5f, 0.0f,
-			 0.0f,  0.5f, 0.0f
+		float vertices[3 * 7] = {
+			-0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f,
+			 0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f,
+			 0.0f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f
 		};
 
 		m_VertexBuffer.reset(VertexBuffer::Create(vertices, sizeof(vertices)));
-		glEnableVertexAttribArray(0);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+
+		{
+			BufferLayout layout = {
+						{ ShaderDataType::Float3, "a_Position"},
+						{ ShaderDataType::Float4, "a_Color"}
+			};
+
+			m_VertexBuffer->SetLayout(layout);
+		}
+
+		uint32_t index = 0;
+		for (const auto& element : m_VertexBuffer->GetLayout())
+		{
+			glEnableVertexAttribArray(index);
+			glVertexAttribPointer(index, 
+				element.GetComponentCount(),
+				ShaderDataTypeToOpenGLBaseType(element.Type),
+				element.Normalized ? GL_TRUE : GL_FALSE,
+				m_VertexBuffer->GetLayout().GetStride(),
+				(const void*)element.Offset);
+			index++;
+		}
+
+
 
 		unsigned int indices[3] = {0, 1, 2};
 		m_IndexBuffer.reset(IndexBuffer::Create(indices, sizeof(indices) / sizeof(uint32_t)));
@@ -50,13 +93,14 @@ namespace Congb {
 			#version 330 core
 			
 			layout(location = 0) in vec3 a_Position;
+			layout(location = 1) in vec4 a_Color;
 			
-			out vec3 v_Position;			
+			out vec4 v_Color;			
 
 			void main()
 			{
 				gl_Position = vec4(a_Position, 1.0);
-				v_Position = a_Position;	
+				v_Color = a_Color;	
 			}
 		)";
 
@@ -64,10 +108,10 @@ namespace Congb {
 			#version 330 core
 			
 			layout(location = 0) out vec4 color;
-			in vec3 v_Position;
+			in vec4 v_Color;
 			void main()
 			{
-				color = vec4(v_Position + 0.5, 1.0);
+				color = v_Color;
 			}
 		)";
 
